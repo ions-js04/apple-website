0c6576f501ca56c0d33f109c7d844733c54b8714
ions-js04 <behnc.natural@gmail.com>
2022-02-25 21:05:56 +0800

    contact page completed

--- src/main.js
+++ src/main.js
@@ -1,19 +1,32 @@
+// Set Nav Title based on document title
+var navTitle = document.querySelector("#nav-title");
+navTitle.innerHTML = document.title;
+
 // Contact us page FAQs dropdown
 
 function dropdown(ID) {
     var questionContainer = document.querySelector("#" + ID); // '#ID' (e.g ID = 'question-1', it would then be '#question-1')
+    var question = questionContainer.querySelector(".question");
     var dropdownText = questionContainer.querySelector(".dropdown-text"); // searches for specified class under its parent only (limited scope)
     var dropdownArrow = questionContainer.querySelector(".dropdown-arrow");
 
     if (dropdownText.classList.contains("hidden")) { // checks if element contains class 'hidden'
         questionContainer.classList.add("expanded");
+        question.classList.add("bold");
         dropdownText.classList.remove("hidden");
         dropdownArrow.src = "img/arrow-up.svg"
     } else {
         questionContainer.classList.remove("expanded");
+        question.classList.remove("bold");
         dropdownText.classList.add("hidden");
         dropdownArrow.src = "img/arrow-down.svg";
     }
+
+    // dropdown animation
+    // var numOfChar= dropdownText.innerHTML.length; // mumber of characters in dropdown texts
+    // for (let i=0; i < numOfChar; i+=10) {
+    //     dropdownText.style.maxHeight += i + "px";
+    // };
 }
 
 // Carousel
