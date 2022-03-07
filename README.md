c505fcdc6fc64dfac4c4255492c3e514096eaea2
ions-js04 <behnc.natural@gmail.com>
2022-03-08 02:30:59 +0800

    reviews page adding filter function, fixing

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # apple-website
+
+References to include:
+- reviews.html: review texts design inspired from https://www.pinterest.com/pin/348817933637561576/
+- apple website ref
--- src/main.js
+++ src/main.js
@@ -10,15 +10,75 @@ function dropdown(ID) {
     var dropdownArrow = questionContainer.querySelector(".dropdown-arrow");
 
     if (dropdownText.classList.contains("hidden")) { // checks if element contains class 'hidden'
-        questionContainer.classList.add("expanded");
-        question.classList.add("bold");
-        dropdownText.classList.remove("hidden");
-        dropdownArrow.src = "img/arrow-up.svg"
+        questionContainer.classList.add("expanded"); // change bg color of dropdown box
+        question.classList.add("bold"); // make question bold when selected
+        dropdownText.classList.remove("hidden"); // make dropdown text appear
+        dropdownArrow.src = "img/arrow-up.svg" // change down arrow to up arrow
     } else {
         questionContainer.classList.remove("expanded");
         question.classList.remove("bold");
         dropdownText.classList.add("hidden");
         dropdownArrow.src = "img/arrow-down.svg";
+    }
+}
+
+var currentSelected = []; // keep track which btn is currently selected (as to not hide other elements too while more than btn is being selected)
+// Reviews Page Filter Elements Shown
+function filter(ID) {
+    var filterBtn = document.querySelector("#" + ID);
+    var allClasses = ["macbook","X","ipad","watch","airpods","tv"];
+    var filterReviews = []; // elements to filter (remove)
+    var elementsToFilter, elementsToRetrieve;
+
+    // get the elements (review box) to hide
+    for (var className of allClasses) { // loops through whole array (allClasses)
+        if (filterBtn.name == className) {
+            currentSelected.push(className);
+        } else {
+            filterReviews.push(className);
+        }
+    }
+
+    // hide or review elements
+    if (filterBtn.classList.contains("selected")) { // if button is selected
+        filterBtn.classList.remove("selected"); // change btn bg color
+
+        for (var a of filterReviews) {
+            for (var b of currentSelected) {
+                if (a == b) {
+                    var index = filterReviews.indexOf(b); // get index of element inside filterReviews array
+                    filterReviews.splice(index,1); // splice(startIndex, amountToDel), deleting the class from filterReviews array
+                } 
+            }
+        }
+
+        // retrieve back hidden elements
+        for (var className of filterReviews) {
+            elementsToRetrieve = document.getElementsByClassName(className); // getElementsByClassName selects ALL elements with matched class name; cannot use query selector as it only selects the first matched element only (not ALL)
+            for (var i=0; i < elementsToRetrieve.length; i++) { // need to use this for loop, as getElementsByClassName returns a nodeList, so need to access every retrieved matched element using index (e.g there are 10 elements with class 'macbook'. so the elementsToFilter at that certain loop is 10)
+                elementsToRetrieve[i].classList.remove("hidden"); // shown elements not inside of filterReviews
+            }
+        }
+    } else {
+        // hide elements
+        filterBtn.classList.add("selected");
+
+        // check whether any elements in filterReviews are also in currentSelected (to remove them from filter list, prevent currently selected choices from being hidden away)
+        for (var a of currentSelected) {
+            for (var b of filterReviews) {
+                if (a == b) {
+                    var index = filterReviews.indexOf(b); // get index of element inside filterReviews array
+                    filterReviews.splice(index,1); // splice(startIndex, amountToDel), deleting the class from filterReviews array
+                } 
+            }
+        }
+
+        for (var className of filterReviews) {
+            elementsToFilter = document.getElementsByClassName(className); // getElementsByClassName selects ALL elements (NOTE: returns in NodeList) with matched class name; cannot use query selector as it only selects the first matched element only (not ALL)
+            for (var i=0; i < elementsToFilter.length; i++) { // need to use this for loop, as getElementsByClassName returns a nodeList, so need to access every retrieved matched element using index (e.g there are 10 elements with class 'macbook'. so the elementsToFilter at that certain loop is 10)
+                elementsToFilter[i].classList.add("hidden"); // shown elements inside of filterReviews
+            }
+        }        
     }
 }
 
