d7a6669f894ad6ae1d9ac1b907465115a08fa455
CRAZYJOKER118 <yewchunchung118@gmail.com>
2022-04-14 21:10:05 +0800

    done

--- src/education.js
+++ src/education.js
@@ -1,16 +1,16 @@
-window.addEventListener("scroll", appearWhenScroll);
-window.addEventListener("scroll", appearOnScroll);
+window.addEventListener("scroll", appearWhenScroll); // add event when scoll
+
 
 function appearWhenScroll() {
 
-    const image = document.querySelector(".picture-mac");
-    const position = image.getBoundingClientRect().top;
-    const screenPosition = window.innerHeight;
+    const image = document.querySelector(".picture-mac"); //point to .picture-mac class name
+    const position = image.getBoundingClientRect().top; // get the position of the screen
+    const screenPosition = window.innerHeight; // get the current position of height
 
-    if (screenPosition > position) {
+    if (screenPosition > position) { // if current position is more than position of screen
 
-        image.classList.add('photo');
+        image.classList.add('photo'); // add class name photo to .picture-mac so the img will appear
     } else {
-        image.classList.remove('photo');
+        image.classList.remove('photo'); // else remove the class name photo
     }
 }
