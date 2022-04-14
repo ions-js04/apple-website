22cc6002d471078dd093be8b70079725c6a4cfaa
bryanwtarc <bryanwbl-wp20@student.tarc.edu.my>
2022-04-14 12:16:02 +0800

    Watch Final

--- src/watch.js
+++ src/watch.js
@@ -1,20 +1,45 @@
-const checkpoint = 100; //set checkpoint for when scroll effect starts
-const endpoint = 500; //set checkpoint for when scroll effect ends
+var x = document.getElementById("se-7-table");
+var y = document.getElementById("SE-table");
+var z = document.getElementById("se-3-table");
 
-
-window.addEventListener("scroll", () => {
-    const currentScroll = window.pageYOffset;
-    opacity = 1;
-
-    //scroll fade out effect
-    if (currentScroll < checkpoint) {
-        opacity = 1;
-    }
-    else if (currentScroll >= checkpoint) {
-        opacity = 1 - currentScroll / endpoint;
-    }
+function showDivS7() {
+    if (x.style.opacity == 0) {
+        x.style.opacity = 1;
+        x.style.lineHeight = 1.5;
+        x.style.height = "50vw";
+    } 
     else {
-        opacity = 0;
+        x.style.lineHeight = 0;
+        x.style.height = 0;
+        x.style.height = 0;
+        x.style.opacity = 0;
     }
-    document.querySelector(".Watch").style.opacity = opacity;
-});
+  }
+
+  function showDivSE() {
+    if (y.style.opacity == 0) {
+        y.style.opacity = 1;
+        y.style.lineHeight = 1.5;
+        y.style.height = "50vw";
+    } 
+    else {
+        y.style.lineHeight = 0;
+        y.style.height = 0;
+        y.style.height = 0;
+        y.style.opacity = 0;
+    }
+  }
+
+  function showDivS3() {
+    if (z.style.opacity == 0) {
+        z.style.opacity = 1;
+        z.style.lineHeight = 1.5;
+        z.style.height = "50vw";
+    } 
+    else {
+        z.style.lineHeight = 0;
+        z.style.height = 0;
+        z.style.height = 0;    
+        z.style.opacity = 0;
+    }
+  }
