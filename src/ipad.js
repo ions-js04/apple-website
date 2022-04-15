678e5562154b5ff926af2e74f9377995db33a52e
bryanwtarc <bryanwbl-wp20@student.tarc.edu.my>
2022-04-15 11:03:46 +0800

    small fixes

--- src/ipad.js
+++ src/ipad.js
@@ -1,6 +1,6 @@
 //Set automation for image carousel
 var pointer = 1;
-setInterval(function(){
+setInterval (function() {
     document.getElementById('btn' + pointer).checked = true;
     pointer ++;
     //if on the 4th img, go back to the 1st
